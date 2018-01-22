c885026c6d93b6a51d261d6fc7ab307d2cefb078
wongca <calvin.kl.wong@gmail.com>
2018-01-23 01:11:59 +0800

    BinaryTreeChecker

--- interview/src/main/java/com/calvin/cake/BinaryTreeChecker.java
+++ interview/src/main/java/com/calvin/cake/BinaryTreeChecker.java
@@ -56,6 +56,173 @@ public class BinaryTreeChecker {
 		return checkIterative(node);
 	}
 
+	/**
+	 * All nodes on the left must be smaller than all nodes on the right
+	 * @param node
+	 * @return -1 if something invalid is found
+	 */
+	public static boolean isValidSearchTree(BinaryTreeNode node){
+		MinMaxResult minMaxAndCheck = getMinMaxAndCheck(node);
+		return minMaxAndCheck.isValid;
+	}
+
+	public static boolean isValidSearchTreeIterative(BinaryTreeNode node){
+		return checkSearchTreeIterative(node);
+	}
+
+	public static boolean isValidSearchTreeInOrder(BinaryTreeNode node){
+		return checkSearchTreeValidInOrder(node);
+	}
+	
+	public static boolean isValidSearchTreeSmart(BinaryTreeNode node){
+		return checkSearchTreeSmart(node, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+	
+	public static class CurrentValue {
+		int value;
+	}
+
+	/**
+	 * I was only able to focus that all nodes on the left of a node must be less than the node.value,
+	 * and all nodes on the right of a node must be greater than the node.value.  However I wasn't able
+	 * to identify that a node is valid if it is within a "range created by its ancestor"
+	 * @param node
+	 * @return
+	 */
+	public static boolean checkSearchTreeSmart(BinaryTreeNode node, int lowerBound, int upperBound){
+		if (node == null){
+			return true;
+		}
+		
+		if (node.value < lowerBound || node.value > upperBound){
+			return false;
+		}
+		
+		return checkSearchTreeSmart(node.left, lowerBound, node.value) &&
+				checkSearchTreeSmart(node.right, node.value, upperBound);
+	}
+	
+	/** There are two ways to do this
+	 *  1) Traverse in-order
+	 * @param node
+	 * @return
+	 */
+	public static boolean checkSearchTreeValidInOrder(BinaryTreeNode node){
+		CurrentValue value = new CurrentValue();
+		value.value = Integer.MIN_VALUE;
+		return checkSearchTreeValidInOrder(node, value);
+	}
+
+	private static boolean checkSearchTreeValidInOrder(BinaryTreeNode node, CurrentValue current){
+		if (node == null){
+			return true;
+		}
+		
+		if (!checkSearchTreeValidInOrder(node.left, current)){
+			return false;
+		}
+		
+		if (node.value <= current.value){
+			return false;
+		}
+		current.value = node.value;
+		
+		if (!checkSearchTreeValidInOrder(node.right, current)){
+			return false;
+		}
+		
+		return true;
+	}
+
+	public static class BinaryTreeNodeInfo {
+		BinaryTreeNode node;
+		BinaryTreeNodeInfo ancestor;
+		boolean leftSideOfAncestor;
+		
+		BinaryTreeNodeInfo(BinaryTreeNode node, BinaryTreeNodeInfo ancestor, boolean leftSideOfAncestor){
+			this.node = node;
+			this.ancestor = ancestor;
+			this.leftSideOfAncestor = leftSideOfAncestor;
+		}
+	}
+	
+	private static boolean checkSearchTreeIterative(BinaryTreeNode node){
+		Stack<BinaryTreeNodeInfo> stack = new Stack<>();
+		stack.push(new BinaryTreeNodeInfo(node, null, false));
+		while (!stack.isEmpty()){
+			BinaryTreeNodeInfo current = stack.pop();
+			
+			// Check if current is ok by going up all the way
+			if (current.ancestor != null){
+				BinaryTreeNodeInfo at = current;
+				while (at.ancestor != null){
+					if (at.leftSideOfAncestor && current.node.value >= at.ancestor.node.value){
+						return false;
+					}
+					if (!at.leftSideOfAncestor && current.node.value <= at.ancestor.node.value){
+						return false;
+					}
+					at = at.ancestor;							
+				}
+			}
+			
+			// Push the children
+			if (current.node.left != null){
+				stack.push(new BinaryTreeNodeInfo(current.node.left, current, true));				
+			}
+			if (current.node.right != null){
+				stack.push(new BinaryTreeNodeInfo(current.node.right, current, false));
+			}
+			
+		}
+		return true;
+	}
+	
+	public static class MinMaxResult {
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		boolean isValid = true;
+		MinMaxResult(int min, int max, boolean isValid){
+			this.min = min;
+			this.max = max;
+			this.isValid = isValid;
+		}
+	}
+	
+	private static MinMaxResult getMinMaxAndCheck(BinaryTreeNode node){
+		if (node.left == null && node.right == null){
+			return new MinMaxResult(node.value, node.value, true);
+		}
+		
+		int min = node.value;
+		int max = node.value;
+		if (node.right != null){
+			MinMaxResult resultRight = getMinMaxAndCheck(node.right);
+			if (!resultRight.isValid){
+				return resultRight;
+			}
+			if (node.value >= resultRight.min){
+				resultRight.isValid = false;
+				return resultRight;
+			}
+			max = resultRight.max;
+		}
+		if (node.left != null){
+			MinMaxResult resultLeft = getMinMaxAndCheck(node.left);
+			if (!resultLeft.isValid){
+				return resultLeft;
+			}
+			if (node.value <= resultLeft.max){
+				resultLeft.isValid = false;
+				return resultLeft;
+			}
+			min = resultLeft.min;
+		}
+				
+		return new MinMaxResult(min, max, true);
+	}
+	
+	
 	private static boolean check(BinaryTreeNode node, int level, Result result){
 		if (node.left == null && node.right == null){
 			result.maxLevel = Math.max(result.maxLevel, level);
--- interview/src/test/java/com/calvin/cake/BinaryTreeCheckerTest.java
+++ interview/src/test/java/com/calvin/cake/BinaryTreeCheckerTest.java
@@ -1,6 +1,7 @@
 package com.calvin.cake;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -19,6 +20,33 @@ public class BinaryTreeCheckerTest {
 		node.left.left.left = new BinaryTreeNode(7);
 		assertFalse(BinaryTreeChecker.isSuperBalanced(node));
 		assertFalse(BinaryTreeChecker.isSuperBalancedIterative(node));
-		
+	}
+	
+	@Test
+	public void testSearchTree(){
+		BinaryTreeNode node = new BinaryTreeNode(10);
+		node.left = new BinaryTreeNode(5);
+		node.right = new BinaryTreeNode(14);
+		node.left.left = new BinaryTreeNode(3);
+		node.left.right = new BinaryTreeNode(7);
+		node.right.left = new BinaryTreeNode(11);
+		node.right.right = new BinaryTreeNode(15);
+		assertTrue(BinaryTreeChecker.isValidSearchTreeSmart(node));
+		assertTrue(BinaryTreeChecker.isValidSearchTreeInOrder(node));
+		assertTrue(BinaryTreeChecker.isValidSearchTreeIterative(node));
+//		assertTrue(BinaryTreeChecker.isValidSearchTree(node));
+//
+		node = new BinaryTreeNode(10);
+		node.left = new BinaryTreeNode(5);
+		node.right = new BinaryTreeNode(14);
+		node.left.left = new BinaryTreeNode(3);
+		node.left.right = new BinaryTreeNode(11);
+		node.right.left = new BinaryTreeNode(11);
+		node.right.right = new BinaryTreeNode(15);
+		assertFalse(BinaryTreeChecker.isValidSearchTreeSmart(node));
+		assertFalse(BinaryTreeChecker.isValidSearchTreeInOrder(node));
+		assertFalse(BinaryTreeChecker.isValidSearchTreeIterative(node));
+//		assertFalse(BinaryTreeChecker.isValidSearchTree(node));
+
 	}
 }
